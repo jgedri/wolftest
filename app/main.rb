a067b86708f772b2b2b435ccba8b6d028d99670b
Jorin G <joring@protonmail.com>
2023-10-02 18:11:15 -0400

    Progress?

--- app/main.rb
+++ app/main.rb
@@ -3,6 +3,7 @@ def select_coat(args)
     rect_color = {r: 0, g: 0, b: 0, a: 255}
     rect_bounds = {x: 100, y: 150, w: 100, h: 100}
     prim_marker = {primitive_marker: :solid}
+    gray_wolf = {x: 100, y: 150, w: 16, h: 16, path: "sprites/wolf/wolf_black_tf.png"}
 
     args.state.coat ||= 0
     args.state.coat_list ||= ["gray", "brown", "black"]
@@ -20,6 +21,7 @@ end
 # Main function.
 def tick args
    # $gtk.download_stb_rb "xenobrain", "rubycolors", "color.rb" if args.state.tick_count == 0
+
 
    args.state.player << { x: 500, y: 500, w: 50, h: 50, }
 
